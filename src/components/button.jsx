ae75681b1a12171a40313dcd72c5431e667f40f7
Néstor Iriondo <nestoririondo@gmail.com>
2023-12-17 18:01:58 +0100

    tweaks to button and keypad

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,6 @@
 const Button = ({ onClick, className, value }) => {
     
     return (
-        
         <button
             onClick={onClick}
             className={className}
--- src/components/keypad.jsx
+++ src/components/keypad.jsx
@@ -3,23 +3,23 @@ import Button from './Button'
 const Keypad = ({ onClick }) => {
 
     const buttons = [
-        { id: 1, value: 7, className: "keypad__key" },
-        { id: 2, value: 8, className: "keypad__key" },
-        { id: 3, value: 9, className: "keypad__key" },
+        { id: 1, value: 7, className: "keypad__key number" },
+        { id: 2, value: 8, className: "keypad__key number" },
+        { id: 3, value: 9, className: "keypad__key number" },
         { id: 4, value: '+', className: "keypad__key operator" },
-        { id: 5, value: 4, className: "keypad__key" },
-        { id: 6, value: 5, className: "keypad__key" },
-        { id: 7, value: 6, className: "keypad__key" },
+        { id: 5, value: 4, className: "keypad__key number" },
+        { id: 6, value: 5, className: "keypad__key number" },
+        { id: 7, value: 6, className: "keypad__key number" },
         { id: 8, value: '-', className: "keypad__key operator" },
-        { id: 9, value: 1, className: "keypad__key" },
-        { id: 10, value: 2, className: "keypad__key" },
-        { id: 11, value: 3, className: "keypad__key" },
+        { id: 9, value: 1, className: "keypad__key number" },
+        { id: 10, value: 2, className: "keypad__key number" },
+        { id: 11, value: 3, className: "keypad__key number" },
         { id: 12, value: '*', className: "keypad__key operator" },
-        { id: 13, value: ',', className: "keypad__key" },
-        { id: 14, value: 0, className: "keypad__key" },
-        { id: 15, value: '=', className: "keypad__key operator" },
+        { id: 13, value: ',', className: "keypad__key comma" },
+        { id: 14, value: 0, className: "keypad__key number" },
+        { id: 15, value: '=', className: "keypad__key equal" },
         { id: 16, value: '/', className: "keypad__key operator" },
-        { id: 17, value: 'C', className: "keypad__key" },
+        { id: 17, value: 'C', className: "keypad__key clear" },
     ]
 
     return (
